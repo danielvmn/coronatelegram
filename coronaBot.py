3696fc26bd81e8b9087e3959a942aa54564a3068
Daniel Negrao <danielvmn@gmail.com>
2020-03-13 20:57:22 +0100

    Added comments and minor adjustments

--- coronaBot.py
+++ coronaBot.py
@@ -13,25 +13,32 @@ from datetime import datetime
 from decouple import config
 from telegram.ext import Updater, CommandHandler
 
+# Environment settings (Telegram chat id / API token)
 telegramToken = config('token')
 chatid = config('chat')
 
+# Logging setup
 format = '%(message)s'
 logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
+# Starts Bot listener to reply to /commands
 def bot_listen():
     updater = Updater(token=telegramToken, use_context=True)
     dispatcher = updater.dispatcher
+    # The handler will call the gemeente method if the /gemeente command is called
     gemeente_handler = CommandHandler('gemeente', gemeente)
     dispatcher.add_handler(gemeente_handler)
+    # Starts polling for commands
     updater.start_polling()
 
 def gemeente(update, context):
     logging.info(f'Replying command "{update.message.text}".')
+    # Extracts the string after '/gemeente' to retrieve the gemeente to search for
     gemeente = update.message.text.replace("/gemeente","")
-    context.bot.sendMessage(chat_id=update.effective_chat.id, text=getStad(gemeente.strip()))
+    context.bot.sendMessage(chat_id=update.effective_chat.id, text=getGemeente(gemeente.strip()))
 
-def getStad(gemeente):
+# Scraps the CSV data from RIVM
+def getGemeente(gemeente):
     url='https://www.rivm.nl/coronavirus-kaart-van-nederland'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -41,8 +48,9 @@ def getStad(gemeente):
     for line in csvReader:
         if line and line[0].casefold() == gemeente.casefold():
             return f'{gemeente} heeft er {line[2]} besmet'
-    return 'Kun niet deze Stad vinden'
+    return f'kan gemeente {gemeente} niet vinden'
 
+# Scaps the total number of infected from RIVM
 def getNumber():
     url = 'https://www.rivm.nl/nieuws/actuele-informatie-over-coronavirus'
     response = requests.get(url)
@@ -51,25 +59,28 @@ def getNumber():
     pattern = r'\d+'
     return int(re.findall(pattern,str(allH2))[1])
 
-bot = Bot(token=telegramToken)
-oldNumber = 0
-
+# Declares and starts the listener thread
 listener = threading.Thread(target=bot_listen)
 listener.daemon = True
 listener.start()
 
+bot = Bot(token=telegramToken)
+
+oldNumber = 0
+
 while True:
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # The updates are usually done at around 14:00, so we only poll around that time.
     if datetime.now().hour > 11 and datetime.now().hour < 16:
         number = getNumber()
         if (number != oldNumber):
             oldNumber = number
-            logging.info(f'{now}: Number changed!')
+            logging.info('Number changed!')
             bot.sendMessage(chat_id=chatid, text=f'Update: {number} positief getest')
             bot.setChatTitle(chat_id=chatid, title=f'Corona Updates Nederland - {number} positief getest')
         else:
-            logging.info(f'{now}: Number not changed')
+            logging.info('Number not changed')
     else:
-        logging.info(f'{now}: Will only run between 12 and 3pm')
+        logging.info('Will only run between 12h and 15h')
 
+    # We don't need to look for new data every moment, so it will poll every 60 seconds
     time.sleep(60)
